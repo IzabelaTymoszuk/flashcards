a0cf53c87e6d0f7c7ff709ac692520414b893775
IzabelaTymoszuk <izabelatymoszuk93@gmail.com>
2020-07-15 22:43:44 +0200

    draft homepage and login

---
+++ app/routes.py
@@ -0,0 +1,34 @@
+from flask import render_template, flash, redirect, url_for
+from app import app
+from app.forms import LoginForm
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Anna'}
+    cards = [
+        {
+            "author": {"username": "John"},
+            "world": "grass",
+            "translation": "trawa",
+            "example": "Don't walk on the grass."
+        },
+        {
+            "author": {"username": "Susan"},
+            "world": "set",
+            "translation": "ustalać, wyznaczać",
+            "example": "Can we set a limit of two hours for the meeting?"
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, cards=cards)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('/index'))
+    return render_template('login.html', title='Sign In', form=form)
