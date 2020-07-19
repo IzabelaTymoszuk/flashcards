430804daaa06523619717ea6c6c048b5473b3187
IzabelaTymoszuk <izabelatymoszuk93@gmail.com>
2020-07-19 18:33:51 +0200

    adding edit profile and errors

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
-from werkzeug.security import generate_password_hash, check_password_hash
-from app import db, login
 from datetime import datetime
+from app import db, login
 from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 @login.user_loader
@@ -14,6 +14,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    cards = db.relationship('Card', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
@@ -30,7 +31,7 @@ class User(UserMixin, db.Model):
 class Card(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     world = db.Column(db.String(64), index=True, unique=True)
-    translate = db.Column(db.String(120))
+    translation = db.Column(db.String(120))
     example = db.Column(db.String(256))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
